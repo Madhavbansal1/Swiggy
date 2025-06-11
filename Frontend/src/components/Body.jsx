74f028461db5761c0ca8d1ee04a66f75b203bba5
Madhavbansal1 <madhavbansal6633@gmail.com>
2025-06-11 11:21:47 +0530

    Initial commit

---
+++ Frontend/src/components/Body.jsx
@@ -0,0 +1,27 @@
+function Body() {
+    return (
+        <>
+            <div className="ml-20 mt-4">
+                <h1 className="text-2xl mb-4">
+                    Restaurants with online food delivery in <span className="text-indigo-900 font-bold">Mathura</span>
+                </h1>
+
+                <button className="border rounded-2xl p-2 mx-4 hover:bg-indigo-900 hover:text-white">
+                    Rating 4.5
+                </button>
+
+                <button className="border rounded-2xl p-2 mx-4 hover:bg-indigo-900  hover:text-white">
+                    Reset
+                </button>
+
+                <input
+                    className="border rounded-2xl p-2 mx-4"
+                    type="text"
+                    placeholder="Search restaurants..."
+                />
+            </div>
+        </>
+    );
+}
+
+export default Body;
